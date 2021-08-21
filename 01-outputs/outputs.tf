89db0dd83814146bb78340bde6b3015c73035df5
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-21 22:15:20 +0530

    samples

---
+++ 01-outputs/outputs.tf
@@ -0,0 +1,11 @@
+output "out1" {
+  value = "Hello World"
+}
+
+output "out2" {
+  value = "How are you doing?"
+}
+
+output "out3" {
+  value = "manita"
+}
---
+++ 03-example1/ec2.tf
@@ -0,0 +1,44 @@
+resource "aws_instance" "sample" {
+  ami                        = "ami-074df373d6bafa625"
+  instance_type              = "t3.micro"
+  vpc_security_groups_ids    = [aws_security_group.allow_ssh.id]
+  tags                       = {
+    Name                     = "Sample"
+  }
+}
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "allow ssh"
+
+  ingress = [
+    {
+      description      = "SSH"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = [0.0.0.0/0]
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
+
+output "ec2-attributes" {
+  value = "aws_instance.sample"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
