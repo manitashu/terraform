555a589b1971c979e220576b18234234c6c62cb1
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-22 10:51:48 +0530

    samples

--- 02-variables/vars.tf
+++ 02-variables/vars.tf
@@ -1,64 +1,70 @@
-variable "sample" {
-  default = "Hello World"
-}
+//variable "sample" {
+//  default = "Hello World"
+//}
+//
+//output "sample" {
+//  value = "var.sample"
+//}
+//
+//output "sample1" {
+//  value = "${var.sample} - From Terraform"
+//}
+//
+//variable "sample2" {}
+//
+//output "sample2" {
+//  value = "var.sample2"
+//}
+//
+//variable "string" {
+//  default = "Hello"
+//}
+//
+//variable "number" {
+//  default = 10
+//}
+//
+//variable "boolean" {
+//  default = false
+//}
+//
+//output "data-types" {
+//  value = "String = ${var.string}, Number = ${var.number} , Boolean = ${var.boolean}"
+//}
+//
+//variable "TRAINING" {
+//  default = "DevOps"
+//}
+//
+//variable "TRAININGS" {
+//  default = [ "Hello" , 100 , true , "World" ]
+//}
+//
+//variable "TRAININGS-DETAILS" {
+//  default = {
+//    AWS = 6
+//    Devops = 8
+//  }
+//}
+//
+//output "TRAINING" {
+//  value = "var.training"
+//}
+//
+//output "FIRST-TRAINING" {
+//  value = "var.TRAININGS[0]"
+//}
+//
+//output "SECOND-TRAINING" {
+//  value = "var.TRAININGS[1]"
+//}
+//
+//output "TRAINING-DETAILS" {
+//  value = "AWS TIMING - ${var.TRAININGS-DETAILS["AWS"]}"
+//}
 
-output "sample" {
-  value = "var.sample"
-}
+variable "FRUITS" {}
 
-output "sample1" {
-  value = "${var.sample} - From Terraform"
-}
-
-variable "sample2" {}
-
-output "sample2" {
-  value = "var.sample2"
-}
-
-variable "string" {
-  default = "Hello"
-}
-
-variable "number" {
-  default = 10
-}
-
-variable "boolean" {
-  default = false
-}
-
-output "data-types" {
-  value = "String = ${var.string}, Number = ${var.number} , Boolean = ${var.boolean}"
-}
-
-variable "TRAINING" {
-  default = "DevOps"
-}
-
-variable "TRAININGS" {
-  default = [ "Hello" , 100 , true , "World" ]
-}
-
-variable "TRAININGS-DETAILS" {
-  default = {
-    AWS = 6
-    Devops = 8
-  }
-}
-
-output "TRAINING" {
-  value = "var.training"
-}
-
-output "FIRST-TRAINING" {
-  value = "var.TRAININGS[0]"
-}
-
-output "SECOND-TRAINING" {
-  value = "var.TRAININGS[1]"
-}
-
-output "TRAINING-DETAILS" {
-  value = "AWS TIMING - ${var.TRAININGS-DETAILS["AWS"]}"
+output "FRUITS" {
+  value = "var.FRUITS"
 }
