84512329c5b94968806a1c298e5e722e80f06ff6
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-22 11:46:39 +0530

    samples

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -11,24 +11,20 @@ resource "aws_security_group" "allow_ssh" {
   name = "allow_ssh"
   description = "allow ssh"
 
-  ingress = [
-    {
-      description      = "SSH"
-      from_port        = 22
-      to_port          = 22
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
+  ingress = {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
-  egress = [
-    {
-      from_port        = 0
-      to_port          = 65535
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-    }
-  ]
+  egress = {
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
 
   tags = {
     Name = "allow_ssh"
