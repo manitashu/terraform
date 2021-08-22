04252bb776f6998d89b8ba8c5b0826817b5f35cb
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-22 11:42:52 +0530

    samples

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,11 +1,11 @@
-//resource "aws_instance" "sample" {
-//  ami                        = "ami-074df373d6bafa625"
-//  instance_type              = "t3.micro"
-//  vpc_security_groups_ids    = [aws_security_group.allow_ssh.id]
-//  tags                       = {
-//    Name                     = "Sample"
-//  }
-//}
+resource "aws_instance" "sample" {
+  ami                        = "ami-074df373d6bafa625"
+  instance_type              = "t3.micro"
+  vpc_security_groups_ids    = [aws_security_group.allow_ssh.id]
+  tags                       = {
+    Name                     = "Sample"
+  }
+}
 
 resource "aws_security_group" "allow_ssh" {
   name = "allow_ssh"
@@ -36,7 +36,7 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 output "ec2-attributes" {
-  value = "aws_security_group.allow_ssh"
+  value = "aws_instance.sample"
 }
 
 provider "aws" {
