e77a4480656f74b1c11717d4ff3add8498ddccd8
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-28 00:28:30 +0530

    terraform

---
+++ roboshop-shell-scripting/ec2.tf
@@ -0,0 +1,16 @@
+//resource "aws_instance" "instances" {
+//  ami           = "ami-074df373d6bafa625"
+//  instance_type = "t3.micro"
+//}
+
+resource "aws_spot_instance_request" "cheap_worker" {
+  COUNT         = lenght(var.COMPONENTS)
+  ami           = "ami-074df373d6bafa625"
+  spot_price    = "0.0031"
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = element(var.COMPONENTS, count.index)
+  }
+}
+
