57ca658ac8f5cc7105128efb2abbbf9b56e35ef7
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-29 11:40:45 +0530

    terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,16 +1,25 @@
 resource "aws_spot_instance_request" "RoboShop" {
-  count         = length(var.COMPONENTS)
-  ami           = "ami-074df373d6bafa625"
-  spot_price    = "0.0031"
-  instance_type = "t3.micro"
+  count                = length(var.COMPONENTS)
+  ami                  = "ami-074df373d6bafa625"
+  spot_price           = "0.0031"
+  instance_type        = "t3.micro"
   wait_for_fulfillment = true
 
-  tags = {
-    Name = element(var.COMPONENTS, count.index)
+  tags                 = {
+    Name               = element(var.COMPONENTS, count.index)
   }
 }
 
+resource "aws_ec2_tag" "name-tag" {
+  count                = length(var.COMPONENTS)
+  resource_id          = element(aws_spot_instance_request.RoboShop.*.spot_instance_id, count.index)
+  key                  = "name"
+  value                = element(var.COMPONENTS, count.index)
+}
 
+output "attributes" {
+  value = aws_spot_instance_request.RoboShop.*.spot_instance_id
+}
 
 
 
