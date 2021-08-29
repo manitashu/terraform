559a4caf0152f4a1995ba7a6099329006c5be3dd
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-30 00:49:04 +0530

    terraform

--- 07-data-source/data.tf
+++ 07-data-source/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "example" {
-  executable_users = ["self"]
+  //executable_users = ["self"]
   most_recent      = true
-  //name_regex       =
+  name_regex       = "^Centos*"
   owners           = ["973714476881"]
 }
 
