67c373a0945ae14963715deb42fce20483f25925
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-29 11:22:17 +0530

    terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -3,11 +3,13 @@ resource "aws_spot_instance_request" "RoboShop" {
   ami           = "ami-074df373d6bafa625"
   spot_price    = "0.0031"
   instance_type = "t3.micro"
+  wait_for_fulfillment = true
 
   tags = {
     Name = element(var.COMPONENTS, count.index)
   }
 }
+
 
 
 
