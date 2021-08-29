ca0bbd1564d0d46dfc5bcb6b61b04846b1af2952
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-30 00:59:27 +0530

    terraform

--- 07-data-source/data.tf
+++ 07-data-source/data.tf
@@ -5,6 +5,24 @@ data "aws_ami" "example" {
   owners           = ["973714476881"]
 }
 
+data "aws_ec2_spot_price" "example" {
+  instance_type     = "t3.micro"
+//  availability_zone = "us-east-1a"
+//
+//  filter {
+//    name   = "product-description"
+//    values = ["Linux/UNIX"]
+//  }
+}
+
 output "amis" {
   value = data.aws_ami.example.id
+}
+
+output "spot" {
+  value = data.aws_ec2_spot_price.example
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
