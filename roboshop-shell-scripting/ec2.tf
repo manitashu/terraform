98024e3c10a80cce2abc9ff84b4a183baf715c11
Manita Kushwaha <manitaen1051@gmail.com>
2021-08-29 22:12:15 +0530

    terraform

--- roboshop-shell-scripting/ec2.tf
+++ roboshop-shell-scripting/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_spot_instance_request" "RoboShop" {
-  count                = length(var.COMPONENTS)
+  count                = local.LENGTH
   ami                  = "ami-074df373d6bafa625"
   spot_price           = "0.0031"
   instance_type        = "t3.micro"
@@ -11,16 +11,38 @@ resource "aws_spot_instance_request" "RoboShop" {
 }
 
 resource "aws_ec2_tag" "name-tag" {
-  count                = length(var.COMPONENTS)
+  count                = local.LENGTH
   resource_id          = element(aws_spot_instance_request.RoboShop.*.spot_instance_id, count.index)
   key                  = "Name"
   value                = element(var.COMPONENTS, count.index)
 }
 
 output "attributes" {
-  value = aws_spot_instance_request.RoboShop.*.spot_instance_id
+  value                = aws_spot_instance_request.RoboShop.*.spot_instance_id
 }
 
+resource "null_resource" "run-shell-scripting" {
+  count                = local.LENGTH
+  provisioner "remote-exec" {
+
+    connection {
+      host             = element(aws_spot_instance_request.RoboShop.*.public_ip, count.index)
+      user             = "centos"
+      password         = "DevOps321"
+    }
+
+    command = [
+      "cd /home/centos",
+      "git clone https://DevOps-Batches@dev.azure.com/DevOps-Batches/DevOps57/_git/shell-scripting"
+      "cd shell-scritping/roboshop",
+      "sudo make ${element(var.COMPONENTS, count.index)}"
+    ]
+  }
+}
+
+locals {
+  LENGTH = length(var.COMPONENTS)
+}
 
 
 
