2c4d13e9ed351454cded64d27b3332b87d5495f8
Manita Kushwaha <manitaen1051@gmail.com>
2021-09-28 16:27:00 +0530

    auto pushing chainges repo

--- 04-remote-state/sample.tf
+++ 04-remote-state/sample.tf
@@ -12,7 +12,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-b57-m"
+    bucket = "terraform-b57-m1"
     key    = "sample/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "terraform"
