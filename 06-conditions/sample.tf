a7a2294fec1732221d445852ad70d4a51450fc42
Manita Kushwaha <manitaen1051@gmail.com>
2021-10-16 07:04:45 +0530

    roboshop

--- 06-conditions/sample.tf
+++ 06-conditions/sample.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   //COUNT         = 0
-  ami           = "ami-074df373d6bafa625"
+  ami           = "ami-0e4e4b2f188e91845"
   instance_type = var.TYPE == null ? "t3.micro" : var.TYPE
   //vpc_security_group_ids     = [aws_security_group.allow_ssh.id]
 
